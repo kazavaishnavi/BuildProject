4a17656057b39a653381920214583904a0f62294
kazavaishnavi <kazavaishnavi010@gmail.com>
2024-07-17 21:14:09 -0500

    code update for character movement

--- client/src/components/GameLoop.js
+++ client/src/components/GameLoop.js
@@ -1,18 +1,20 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
-import {connect} from 'react-redux';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { connect, useDispatch } from 'react-redux';
 import CanvasContext from './CanvasContext';
-
-import {MOVE_DIRECTIONS, MAP_DIMENSIONS, TILE_SIZE} from './mapConstants';
+import { MOVE_DIRECTIONS, MAP_DIMENSIONS, TILE_SIZE } from './mapConstants';
 import { MY_CHARACTER_INIT_CONFIG } from './characterConstants';
-import {checkMapCollision} from './utils';
+import { checkMapCollision } from './utils';
+import { update } from './slices/allCharactersSlice';
 
-const GameLoop = ({children, allCharactersData}) => {
+const GameLoop = ({ children, allCharactersData }) => {
     const canvasRef = useRef(null);
     const [context, setContext] = useState(null);
+    const dispatch = useDispatch();
+
     useEffect(() => {
         // frameCount used for re-rendering child components
         console.log("initial setContext");
-        setContext({canvas: canvasRef.current.getContext('2d'), frameCount: 0});
+        setContext({ canvas: canvasRef.current.getContext('2d'), frameCount: 0 });
     }, [setContext]);
 
     // keeps the reference to the main rendering loop
@@ -20,17 +22,33 @@ const GameLoop = ({children, allCharactersData}) => {
     const mycharacterData = allCharactersData[MY_CHARACTER_INIT_CONFIG.id];
 
     const moveMyCharacter = useCallback((e) => {
-        var currentPosition = mycharacterData.position;
+        
+        var currentPosition = mycharacterData.position ; 
         const key = e.key;
         if (MOVE_DIRECTIONS[key]) {
             // ***********************************************
             // TODO: Add your move logic here
+
+            const [dx, dy] = MOVE_DIRECTIONS[key];
+            const newPosition = {
+                x: currentPosition.x + dx,
+                y: currentPosition.y + dy,
+            };
+            
+            if (!checkMapCollision(newPosition.x, newPosition.y) ) {
+              const updatedUsers = {
+                    allCharactersData,
+                    [MY_CHARACTER_INIT_CONFIG.id]: { mycharacterData, position: newPosition }
+                };
+                dispatch(update(updatedUsers));
+             
+            }
         }
     }, [mycharacterData]);
 
     const tick = useCallback(() => {
         if (context != null) {
-            setContext({canvas: context.canvas, frameCount: (context.frameCount + 1) % 60});
+            setContext({ canvas: context.canvas, frameCount: (context.frameCount + 1) % 60 });
         }
         loopRef.current = requestAnimationFrame(tick);
     }, [context]);
@@ -40,7 +58,7 @@ const GameLoop = ({children, allCharactersData}) => {
         return () => {
             loopRef.current && cancelAnimationFrame(loopRef.current);
         }
-    }, [loopRef, tick])
+    }, [loopRef, tick]);
 
     useEffect(() => {
         document.addEventListener('keypress', moveMyCharacter);
@@ -52,7 +70,7 @@ const GameLoop = ({children, allCharactersData}) => {
     return (
         <CanvasContext.Provider value={context}>
             <canvas
-                ref={canvasRef} 
+                ref={canvasRef}
                 width={TILE_SIZE * MAP_DIMENSIONS.COLS}
                 height={TILE_SIZE * MAP_DIMENSIONS.ROWS}
                 class="main-canvas"
@@ -63,7 +81,7 @@ const GameLoop = ({children, allCharactersData}) => {
 };
 
 const mapStateToProps = (state) => {
-    return {allCharactersData: state.allCharacters.users};
+    return { allCharactersData: state.allCharacters.users };
 };
 
 export default connect(mapStateToProps, {})(GameLoop);
