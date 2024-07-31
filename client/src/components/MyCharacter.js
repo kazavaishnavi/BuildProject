a4a31dabe0398c444546ab35fbf7c847823c01da
kazavaishnavi <kazavaishnavi010@gmail.com>
2024-07-31 08:38:04 -0500

    updating the code so that each character see the movement of other character

--- client/src/components/MyCharacter.js
+++ client/src/components/MyCharacter.js
@@ -1,27 +1,28 @@
-import React, { useEffect, useContext } from 'react';
-import { connect } from 'react-redux';
+import React, {useEffect, useContext} from 'react';
+import {connect} from 'react-redux';
+
 import CanvasConext from './CanvasContext';
-import { CHARACTER_IMAGE_SIZE, CHARACTER_CLASSES_MAP } from './characterConstants';
-import { TILE_SIZE } from './mapConstants';
-import { loadCharacter } from './slices/statusSlice';
+import {CHARACTER_IMAGE_SIZE, CHARACTER_CLASSES_MAP} from './characterConstants';
+import {TILE_SIZE} from './mapConstants';
+import {loadCharacter} from './slices/statusSlice';
 import { MY_CHARACTER_INIT_CONFIG } from './characterConstants';
-import { update as updateAllCharactersData } from './slices/allCharactersSlice';
+import {update as updateAllCharactersData} from './slices/allCharactersSlice'
 import { firebaseDatabase } from "./firebase/firebase";
 import { set, ref } from "firebase/database";
 
-const MyCharacter = ({ myCharactersData, loadCharacter, webrtcSocket }) => {
-    const context = useContext(CanvasConext);
 
+function MyCharacter({ myCharactersData, loadCharacter, updateAllCharactersData, webrtcSocket }) {
+    const context = useContext(CanvasConext);
     useEffect(() => {
         const myInitData = {
             ...MY_CHARACTER_INIT_CONFIG,
             socketId: webrtcSocket.id,
         };
-        
+
         const users = {};
         const myId = MY_CHARACTER_INIT_CONFIG.id;
         users[myId] = myInitData;
-
+        //updateAllCharactersData(users);
         const dbRef = ref(firebaseDatabase, `users/${MY_CHARACTER_INIT_CONFIG.id}`);
         set(dbRef, myInitData);
        return () => {
@@ -31,8 +32,9 @@ const MyCharacter = ({ myCharactersData, loadCharacter, webrtcSocket }) => {
     }, [webrtcSocket]);
 
     useEffect(() => {
-        if (!context || !myCharactersData) return;
-
+        if (context == null || myCharactersData == null) {
+            return;
+        }
         const characterImg = document.querySelector(`#character-sprite-img-${MY_CHARACTER_INIT_CONFIG.characterClass}`);
         const { sx, sy } = CHARACTER_CLASSES_MAP[MY_CHARACTER_INIT_CONFIG.characterClass].icon;
         context.canvas.drawImage(
@@ -47,15 +49,15 @@ const MyCharacter = ({ myCharactersData, loadCharacter, webrtcSocket }) => {
             CHARACTER_IMAGE_SIZE
         );
         loadCharacter(true);
-    }, [context, myCharactersData, loadCharacter]);
+    }, [context, myCharactersData?.position.x, myCharactersData?.position.y, loadCharacter]);
 
     return null;
-};
+}
 
 const mapStateToProps = (state) => {
-    return { myCharactersData: state.allCharacters.users[MY_CHARACTER_INIT_CONFIG.id] };
+    return {myCharactersData: state.allCharacters.users[MY_CHARACTER_INIT_CONFIG.id]};
 };
 
-const mapDispatch = { loadCharacter, updateAllCharactersData };
+const mapDispatch = {loadCharacter, updateAllCharactersData};
 
 export default connect(mapStateToProps, mapDispatch)(MyCharacter);
